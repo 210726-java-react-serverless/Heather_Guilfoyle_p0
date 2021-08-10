81be35b7e00de20f62e62ff0fc5d29c97232adf3
hguilfoyle <hguilfoyle1@gmail.com>
2021-08-10 09:28:08 -0400

    Created new UserSchedule POJO and new repository to put student schedules into

--- src/main/java/com/revature/ncu/models/UserSchedule.java
+++ src/main/java/com/revature/ncu/models/UserSchedule.java
@@ -1,2 +1,66 @@
-package com.revature.ncu.models;public class UserSchedule {
+package com.revature.ncu.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserSchedule {
+
+    private String courseID;
+    private String courseName;
+    private String meetDay;
+    private String meetTime;
+
+    public UserSchedule(String courseID, String courseName, String meetDay, String meetTime) {
+        this.courseID = courseID;
+        this.courseName = courseName;
+        this.meetDay = meetDay;
+        this.meetTime = meetTime;
+    }
+    
+    //for Jackson
+    public UserSchedule(){
+        super();
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getMeetDay() {
+        return meetDay;
+    }
+
+    public void setMeetDay(String meetDay) {
+        this.meetDay = meetDay;
+    }
+
+    public String getMeetTime() {
+        return meetTime;
+    }
+
+    public void setMeetTime(String meetTime) {
+        this.meetTime = meetTime;
+    }
+
+    @Override
+    public String toString() {
+        return "UserSchedule{" +
+                "courseID='" + courseID + '\'' +
+                ", courseName='" + courseName + '\'' +
+                ", meetDay='" + meetDay + '\'' +
+                ", meetTime='" + meetTime + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/revature/ncu/repositories/UserCoursesRepository.java
+++ src/main/java/com/revature/ncu/repositories/UserCoursesRepository.java
@@ -1,2 +1,26 @@
-package com.revature.ncu.repositories;public class UserCoursesRepository {
+package com.revature.ncu.repositories;
+
+import com.revature.ncu.models.UserSchedule;
+
+public class UserCoursesRepository implements CrudRepository<UserSchedule>{
+
+    @Override
+    public UserSchedule findById(int id) {
+        return null;
+    }
+
+    @Override
+    public UserSchedule save(UserSchedule newResource) {
+        return null;
+    }
+
+    @Override
+    public boolean update(UserSchedule updatedResource) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return false;
+    }
 }
